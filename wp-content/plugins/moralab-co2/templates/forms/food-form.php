d69c7fabd639b1f39f9bdc0b99652930e0209260
syborg101 <synishigata101@yahoo.com>
2016-07-03 12:03:54 -1000

    Food form update

--- wp-content/plugins/moralab-co2/templates/forms/food-form.php
+++ wp-content/plugins/moralab-co2/templates/forms/food-form.php
@@ -21,36 +21,45 @@ $food = get_post_meta($carbon_data['ID'], 'food_data', true);
 	</div>
 	<!-- End Sy's Edits -->
 	
-	<!-- Sy's Edits:  Added an outer div (id=hiddenfoodinput), so food input is hidden by default -->
+	<!-- Sy's Edits:  -Added an outer div (id=hiddenfoodinput), so food input is hidden by default 
+					  -Added extra divs (row and columns) so every food type can be on the same row 
+					  -Made food type labels and inputs centered instead of left-aligned -->
 	<div hidden id="hiddenfoodinput">
-	
 		<div><p>How many servings do you eat?</p></div>
 
-		<div class="input-group">
-			<span class="input-group-addon lamb"> Lamb </span>
-			<input type="text" min="0" name="lamb" onchange="carbon_food()" id="lamb" class="form-control" value="<?php echo !empty($carbon_data)? $food['lamb']:'';?>">
+		<div class="row">
+			<div class="small-2 columns">
+				<div class="input-group" style="text-align: center">
+					<span class="input-group-addon lamb"> Lamb </span>
+					<input style="text-align: center" type="text" min="0" name="lamb" onchange="carbon_food()" id="lamb" class="form-control" value="<?php echo !empty($carbon_data)? $food['lamb']:'';?>">
+				</div>
+			</div>
+			<div class="small-2 columns">
+				<div class="input-group" style="text-align: center">
+					<span class="input-group-addon beef"> Beef </span>
+					<input style="text-align: center" type="text" min="0" name="beef" onchange="carbon_food()" id="beef" class="form-control" value="<?php echo !empty($carbon_data)? $food['beef']:'';?>">
+				</div>
+			</div>
+			<div class="small-2 columns">
+				<div class="input-group" style="text-align: center">
+					<span class="input-group-addon pork"> Pork </span>
+					<input style="text-align: center" type="text" min="0" name="pork" onchange="carbon_food()" id="pork" class="form-control" value="<?php echo !empty($carbon_data)? $food['pork']:'';?>">
+				</div>
+			</div>
+			<div class="small-2 columns">
+				<div class="input-group" style="text-align: center">
+					<span class="input-group-addon fish"> Fish </span>
+					<input style="text-align: center" type="text" min="0" name="fish" onchange="carbon_food()" id="fish" class="form-control" value="<?php echo !empty($carbon_data)? $food['fish']:'';?>">
+				</div>
+			</div>
+			<div class="small-2 columns">
+				<div class="input-group" style="text-align: center">
+					<span class="input-group-addon poultry"> Poultry </span>
+					<input style="text-align: center" type="text" min="0" name="poultry" onchange="carbon_food()" id="poultry" class="form-control" value="<?php echo !empty($carbon_data)? $food['poultry']:'';?>">
+				</div>
+			</div>
 		</div>
-
-		<div class="input-group">
-			<span class="input-group-addon beef"> Beef </span>
-			<input type="text" min="0" name="beef" onchange="carbon_food()" id="beef" class="form-control" value="<?php echo !empty($carbon_data)? $food['beef']:'';?>">
-		</div>
-
-		<div class="input-group">
-			<span class="input-group-addon pork"> Pork </span>
-			<input type="text" min="0" name="pork" onchange="carbon_food()" id="pork" class="form-control" value="<?php echo !empty($carbon_data)? $food['pork']:'';?>">
-		</div>
-
-		<div class="input-group">
-			<span class="input-group-addon fish"> Fish </span>
-			<input type="text" min="0" name="fish" onchange="carbon_food()" id="fish" class="form-control" value="<?php echo !empty($carbon_data)? $food['fish']:'';?>">
-		</div>
-
-		<div class="input-group">
-			<span class="input-group-addon poultry"> Poultry </span>
-			<input type="text" min="0" name="poultry" onchange="carbon_food()" id="poultry" class="form-control" value="<?php echo !empty($carbon_data)? $food['poultry']:'';?>">
-		</div>
-
+		
 		<div class="input-group">               
 			<input type="hidden" name="co2_food" id="co2_food" value="<?php echo !empty($carbon_data)? $food['co2_food']:'';?>">
 		</div>
